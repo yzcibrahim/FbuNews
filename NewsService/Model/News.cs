167930d663332239b4a90bd0c0dbd14662e3dcc2
karakuserdi <karakuserdi@gmail.com>
2022-03-25 13:48:14 +0300

    NewsService

---
+++ NewsService/Model/News.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NewsService.Model
+{
+    public class News
+    {
+        public int Id { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int CreateUserId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Brief { get; set; }
+
+        public string Content { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public bool IsShowOnSlide { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public int ReadCount { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+    }
+}
---
+++ NewsService/NewsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewsService.Model;
+
+namespace NewsService
+{
+    public class NewsDbContext : DbContext
+    {
+        public NewsDbContext(DbContextOptions<NewsDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<News> News { get; set; }
+    }
+}
