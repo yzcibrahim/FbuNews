0133165068495718514dac09b3011dde2b475748
fbgoktug7@hotmail.com <fbgoktug7@hotmail.com>
2022-03-25 12:09:46 +0300

    CommentServiceAdded

---
+++ CommentsService/Dtos/CommentDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommentsService.Dtos
+{
+    public class CommentDto
+    {
+        public int Id { get; set; }
+        public string CommentText { get; set; }
+        public int ApprovalId { get; set; }
+        public DateTime CreateDate { get; set; }
+        public int CreateUserId { get; set; }
+
+
+    }
+}
---
+++ CommentsService/Migrations/20220324111426_comment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CommentsService.Migrations
+{
+    public partial class comment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CommentText = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ApprovalId = table.Column<int>(type: "int", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreateUserId = table.Column<int>(type: "int", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+        }
+    }
+}
